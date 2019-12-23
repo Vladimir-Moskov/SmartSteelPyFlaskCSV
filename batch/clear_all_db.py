3ceac307ff16a2aebde4de714e5b870beec5ebfd
Volodymyr <boba.mockob@gmail.com>
2019-12-23 09:07:01 -0500

    Update clear_all_db.py

--- batch/clear_all_db.py
+++ batch/clear_all_db.py
@@ -1,8 +1,18 @@
-# clear data base in order to do full testing of data extract
+"""
+Use this script to clean data base in order to do full testing of data extract.
+"""
 
 from app.models import SteelProcessing
 from app.models import ApplicationRequestLog
 
-if __name__ == '__main__':
+
+def clean_db():
+    """
+      Run clean DB by using Flask ecosystem - just call model interface
+    """
     ApplicationRequestLog.query_delete_all()
     SteelProcessing.query_delete_all()
+
+    
+if __name__ == '__main__':
+   clean_db()
