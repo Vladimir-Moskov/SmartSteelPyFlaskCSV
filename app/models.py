f7782d5dd74f8e0dbcdb9e9ccf3ac25b1ce565f9
Vladimir-Moskov <boba.mockob@gmail.com>
2019-12-27 16:02:55 -0500

    another comments

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class ApplicationRequestLog(db.Model):
         Table to store user requests for "On each GET request, log that the data was requested (in the same database)"
     """
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    timestamp = db.Column(db.DateTime, index=True, unique=False, default=datetime.datetime.now)
+    timestamp = db.Column(db.DateTime, index=True, unique=False, default=datetime.now())
     remote_addr = db.Column(db.String(100))
     url = db.Column(db.String(1000))
     method = db.Column(db.String(10))
@@ -24,38 +24,39 @@ class ApplicationRequestLog(db.Model):
         Standard customization of class instance to string
         :return: string representation of object
         """
-        return '<ApplicationRequestLog: id:{}, timestamp:{}, remote_address:{}, remote_user:{}, user_agent:{}>'.\
-            format(self.id, self.timestamp, self.remote_address, self.url, self.remote_user, self.user_agent)
+        return f'<ApplicationRequestLog: id:{self.id}, timestamp:{self.timestamp}, ' \
+               f'remote_address:{self.remote_address}, url:{self.url}, remote_user:{self.remote_user},' \
+               f' user_agent:{self.user_agent}>'
 
-    @staticmethod
-    def query_get_all():
+    @classmethod
+    def query_get_all(cls):
         """
         just select all
         :return: all records from db (no paging or filtering)
         """
-        return ApplicationRequestLog.query.all()
+        return cls.query.all()
 
-    @staticmethod
-    def query_delete_all():
+    @classmethod
+    def query_delete_all(cls):
         """
             just delete all records from table - for testing purpose only
 
         """
-        ApplicationRequestLog.query.delete()
+        cls.query.delete()
         db.session.commit()
 
-    @staticmethod
-    def query_add(request):
+    @classmethod
+    def query_add(cls, request):
         """
         add log record into DB, simple as possible
         :param request: real request - GET http from browser, web page
         :return: nothing
         """
-        new_row = ApplicationRequestLog(remote_addr=request.remote_addr,
-                                        url=request.url,
-                                        method=request.method,
-                                        user_agent=str(request.user_agent),
-                                        remote_user=request.remote_user)
+        new_row = cls(remote_addr=request.remote_addr,
+                      url=request.url,
+                      method=request.method,
+                      user_agent=str(request.user_agent),
+                      remote_user=request.remote_user)
         db.session.add(new_row)
         db.session.commit()
 
@@ -85,8 +86,8 @@ class SteelProcessing(db.Model):
             Standard customization of class instance to string
             :return: string representation of object
         """
-        return '<SteelProcessing: id:{}, timestamp:{}, temperature:{}, duration:{}>'.\
-            format(self.id, self.timestamp, self.temperature, self.duration)
+        return f'<SteelProcessing: id:{self.id}, timestamp:{self.timestamp}, temperature:{self.temperature}, ' \
+               f'duration:{self.duration}>'
 
     # TODO - change it to dictionary {header: field}
     @staticmethod
@@ -97,24 +98,24 @@ class SteelProcessing(db.Model):
         """
         return ["ID", "TIMESTAMP", "TEMPERATURE", "DURATION"]
 
-    @staticmethod
-    def query_get_all():
+    @classmethod
+    def query_get_all(cls):
         """
           just select all
           :return: all records from db (no paging or filtering)
         """
-        return SteelProcessing.query.all()
+        return cls.query.all()
 
-    @staticmethod
-    def query_delete_all():
+    @classmethod
+    def query_delete_all(cls):
         """
             just delete all records from table - for testing purpose only
         """
-        SteelProcessing.query.delete()
+        cls.query.delete()
         db.session.commit()
 
-    @staticmethod
-    def query_add_by_id(row_id, timestamp, temperature, duration):
+    @classmethod
+    def query_add_by_id(cls, row_id, timestamp, temperature, duration):
         """
            add log record into DB, simple as possible
         :param row_id: row data id
@@ -125,7 +126,7 @@ class SteelProcessing(db.Model):
         :return: nothing
         """
         # check if row with the same id already exists
-        exists = SteelProcessing.query.filter_by(id=row_id).first()
+        exists = cls.query.filter_by(id=row_id).first()
         if not exists:
             # TODO: convert timedelta to Float in order to save it in DB
             # from datetime import timedelta
@@ -136,7 +137,7 @@ class SteelProcessing(db.Model):
             # duration_microsec = dt.microseconds
             #
             timestamp_datetime = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-            new_row = SteelProcessing(id=row_id, timestamp=timestamp_datetime, temperature=temperature, duration=duration)
+            new_row = cls(id=row_id, timestamp=timestamp_datetime, temperature=temperature, duration=duration)
             db.session.add(new_row)
             db.session.commit()
         return exists
